54ae2548ab40598024ab68075120236cc2b8a113
DESKTOP-OMQTFCT\vefetch <14cse11@gmail.com>
2016-09-10 17:42:51 +0530

    updated bill no

--- controller/get_bill.php
+++ controller/get_bill.php
@@ -3,8 +3,9 @@
   $db = db_connect();
   $sql = "SELECT `id`,`items` FROM `purchase_details` WHERE `date` BETWEEN '".$_POST['start_date']."' AND '".$_POST['end_date']."' ";
   $result = execute_query($sql, $db);
-   $f_val = "";
-   $i = 0;
+  $f_val = "";
+  $i = 0;
+  $total = 0;
   while ($row = $result->fetch_assoc()) {
           $final[] = $row;
         }
@@ -19,8 +20,10 @@
                 <td>SE".$final_data['id']."</td>
                 <td>".$final_data['data']['item_name1'].",...</td>
                 <td>".$final_data['data']['grand_total']."</td></tr>";
+                $total =   $total + $final_data['data']['grand_total'];
                 
         }
+        $f_val = $f_val . "<tr><td>Grand Total:".$total."</td></tr>";
         
         echo $f_val;
 
