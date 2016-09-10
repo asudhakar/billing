3e8480e70a2e3edfad5f427d98d0857b5d42c362
sudhakar <asudhakar@live.in>
2016-09-10 16:35:38 +0530

    final updated with mobile number retrival

--- view/get_mobile_no.php
+++ view/get_mobile_no.php
@@ -2,9 +2,9 @@
 	include_once '../model/db.php';
 	$db = db_connect();
 	$searchTerm = $_GET['term'];
-	$query = $db->query("SELECT * FROM customers WHERE phone_number LIKE '%".$searchTerm."%' ORDER BY phone_number ASC");
+	$sql = "SELECT * FROM customers WHERE phonenumber LIKE '%".$searchTerm."%' ORDER BY phonenumber ASC";
+	$query = $db->query($sql);
 	while ($row = $query->fetch_assoc()) {
-	    $data[] = $row['phone_number'];
+	    $data[] = $row['phonenumber'];
 	}
-
 	echo json_encode($data);
