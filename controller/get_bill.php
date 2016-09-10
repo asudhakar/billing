9ae5aeff1741a5061b66dc328c4b41257ef92a59
DESKTOP-OMQTFCT\vefetch <14cse11@gmail.com>
2016-09-10 16:36:09 +0530

    no message

--- controller/get_bill.php
+++ controller/get_bill.php
@@ -1,29 +1,27 @@
 <?php 
-	include_once '../model/db_func.php';
-  $dbHost = 'localhost';
-  $dbUsername = 'root';
-  $dbPassword = '';
-  $dbName = 'bill';
-  $db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+	include_once '../model/db.php';
+  $db = db_connect();
   $sql = "SELECT `id`,`items` FROM `purchase_details` WHERE `date` BETWEEN '".$_POST['start_date']."' AND '".$_POST['end_date']."' ";
-  $result = $db->query($sql);
+  $result = execute_query($sql, $db);
+   $f_val = "";
+   $i = 0;
   while ($row = $result->fetch_assoc()) {
           $final[] = $row;
         }
         foreach ($final as $value) {
-
-          $unserialized_values[] = unserialize($value['items']);
-        } 
-        
-        $f_val = "";
-        foreach ($unserialized_values as $final_data) {           
-          $f_val = $f_val."<tr>
-                <td>".$final_data['sno1']."</td>
-                <td>".$final_data['item_name1'].",...</td>
-                <td>".$final_data['grand_total']."</td>  
-          </tr>";
+         
+                $unserialized_values[$i]['id'] = $value['id'];
+            $unserialized_values[$i]['data'] = unserialize($value['items']);
+            $i++;
+          }
+        foreach ($unserialized_values as $final_data) {  
+                $f_val = $f_val."<tr>
+                <td>SE".$final_data['id']."</td>
+                <td>".$final_data['data']['item_name1'].",...</td>
+                <td>".$final_data['data']['grand_total']."</td></tr>";
+                
         }
-
+        
         echo $f_val;
 
 
